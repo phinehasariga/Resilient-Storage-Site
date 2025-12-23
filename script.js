63985cb05ba7847f9d180cc63b8dbfee00ada5b9
ARIGA PHINEHAS <phinehasariga@gmail.com>
2025-12-23 14:12:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function triggerDownload() {
+    // Wait 1 second, then show a thank you message
+    setTimeout(() => {
+        alert("Thanks for downloading! \n\nPlease run the installer 'ResilientStorage_Setup_v7.exe' to finish setting up.");
+    }, 1000);
+}
+
+console.log("Resilient Storage Website Loaded - v7.0");
